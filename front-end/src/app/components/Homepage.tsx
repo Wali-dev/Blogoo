7d0f4ef1447c7e3d50077f3b6864947689ecb8e0
Wali Cornell <cornelhassan780@gmail.com>
2024-05-10 20:18:29 +0530

    card styling is done

--- front-end/src/app/components/Homepage.tsx
+++ front-end/src/app/components/Homepage.tsx
@@ -1,20 +1,33 @@
+"use client"
 import fakedata from '@/app/Data/fakedata.json';
 const blogs = fakedata.blog_posts;
 const Homepage = () => {
+
+    const dateFormatChanger = (date: string) => {
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var now = new Date(date);
+        return (months[now.getMonth()] + ' ' + now.getDate() + ', ' + now.getFullYear()); //Tuesday February 12 2013
+    }
+
+
+
     return (
-        <div className="h-auto md:h-[40rem] ">
-            <div className='grid sm:grid-cols-3 gap-x-0'>
+        <div className="h-auto md:h-[40rem] flex justify-center">
+            <div className='grid sm:grid-cols-3 grid-cols-1 gap-x-3'>
                 {blogs.map(blog => (
-                    <div className="w-96 bg-base-100" key={blog.id}>
-                        <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="">
-                            <h2 className="card-title">{blog.title}</h2>
-                            <p>{blog.full_blog.substring(0, 100)}...</p>
-                            <div>
-                                <div>{blog.author_name}</div>
-                                <div>{blog.created_at.substring(0, 10)}</div>
+                    <div className="sm:w-80 bg-base-100 m-3" key={blog.id}>
+                        <a href='/'>
+                            <figure><img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="">
+                                <h2 className="card-title my-3">{blog.title}</h2>
+                                <p className='text-gray-400 mb-2'>{blog.full_blog.substring(0, 80)}...</p>
+                                <div>
+                                    <a href='/' className='text-orange-400 text-sm'>{blog.author_name}</a>
+                                    <div className='text-xs text-gray-400'>{dateFormatChanger(blog.created_at)}</div>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 ))}
             </div>
